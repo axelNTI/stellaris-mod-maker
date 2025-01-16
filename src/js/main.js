eb95071f2a369e3c1cd62df65505ef0ce1b6bf0b
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-01-16 09:50:22 +0100

    Cleaned up the last of the switch to compiled html

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 $(async () => {
-   $("body").append("<h1>Hello World</h1>");
-
    const mods = await backend.getModDescriptorNames();
 
    mods.forEach(async (mod) => {
       const name = await backend.getModName(mod);
       const folder = await backend.getModFolder(mod);
       $("body").append(`<p>${name}</p>`);
+      $("body").append("<button>Open</button>");
    });
 });
